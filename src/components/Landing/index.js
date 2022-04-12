3c3de002416d9feb7174debffeb00e523a6cb77a
David Mejia <david7mejia@gmail.com>
2022-04-12 16:19:46 -0500

    add landing header styles

---
+++ src/components/Landing/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./Landing.css";
+
+const Landing = () => {
+
+
+    return (
+      <div>
+        <div className="arbilex">
+          <p>ArbiLex Frontend Exercise</p>
+        </div>
+        {/* Hi there from inside */}
+      </div>
+    );
+
+}
+
+
+export default Landing
