4e6654bd63e3985753e23d18fae792139a1d61ff
David Mejia <david7mejia@gmail.com>
2022-04-15 17:42:30 -0500

    data visualization pt 1

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -1,13 +1,15 @@
 import Design from "../Design/index";
 import Interaction from "../Interaction";
-import './Content.css';
+import Data from "../Data";
+import "./Content.css";
 
 const Content = () => {
   return (
-    <div className='main-content-holder'>
-      <div className='main-content'>
-      <Design />
-      <Interaction />
+    <div className="main-content-holder">
+      <div className="main-content">
+        <Design />
+        <Interaction />
+        <Data />
       </div>
     </div>
   );
