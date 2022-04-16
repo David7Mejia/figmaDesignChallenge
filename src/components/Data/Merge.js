719a20fd214326a225afe8d8c140dbaa19050363
David Mejia <david7mejia@gmail.com>
2022-04-16 16:31:45 -0500

    comment and refactor functions in Merge component

--- src/components/Data/Merge.js
+++ src/components/Data/Merge.js
@@ -2,11 +2,12 @@ import React, { useEffect, useState, useRef } from "react";
 import * as d3 from "d3";
 
 const Merge = ({ justiceData, casesData }) => {
-  // console.log({justiceData, casesData});
   const [count, setCount] = useState([]);
   const [merged, setMerged] = useState([]);
+  const [time, setTime] = useState([]);
 
   useEffect(() => {
+    // Count the number of cases per justice and save to object
     let casesCount = (casesData) => {
       let cases = {};
       casesData.forEach((el) => {
@@ -19,7 +20,7 @@ const Merge = ({ justiceData, casesData }) => {
       return cases;
     };
     setCount(casesCount(casesData));
-
+    // Get count and Justice data and merge into one object with points being coordinates on plane
     const mergeData = (justiceData, count) => {
       let mergedData = {};
       justiceData.forEach((el) => {
@@ -32,32 +33,79 @@ const Merge = ({ justiceData, casesData }) => {
       });
       return mergedData;
     };
+    // Set time data to one array for parsing in D3
+    const timeData = (justiceData) => {
+      let times = [];
+      justiceData.forEach((el) => {
+        times.push(el[1]);
+      });
+      return times;
+    };
+    setTime(timeData(justiceData));
 
     setMerged(mergeData(justiceData, count));
   }, [casesData, justiceData]);
 
   const svgRef = useRef();
+    const ess = [];
+    // Create array with count of cases per justice and the date they joined from the merged data
+    // merged data has id of justice to identify who voted => unnecessary for D3
+  for (let key in merged) {
+    let x = merged[key].points[0];
+    let y = merged[key].points[1];
+    ess.push([x, y]);
+  }
+  console.log(ess);
 
-  useEffect(() => {
-    const margin = { top: 20, right: 20, bottom: 30, left: 40 };
-    const width =
-      parseInt(d3.select(svgRef.current).style("width")) -
-      margin.left -
-      margin.right;
-    const height =
-      parseInt(d3.select(svgRef.current).style("height")) -
-      margin.top -
-        margin.bottom;
+  //   useEffect(() => {
+  //     //container
+  //     // const parseTime = d3.timeParse("%b %d, %Y");
+  //     const w = 1000;
+  //       const h = 1000;
 
-      const svg = d3
-          .select(svgRef.current)
-          .attr("width", width + margin.left + margin.right)
-          .attr("height", height + margin.top + margin.bottom)
+  //       var svg = d3.select(svgRef.current);
 
-        
+  //       const mDomain = d3.extent(time);
+  //       const xScale = d3
+  //       .scaleTime()
+  //       .domain(mDomain)
+  //           .range([0, w]);
 
+  //       const yScale = d3
+  //           .scaleLinear()
+  //           .domain([0, d3.max(count)])
+  //           .range([h, 0]);
 
-  }, []);
+  //       var xAxis = d3.axisBottom(xScale);
+  //       svg.append("g").attr("transform", "translate(0,60)").call(xAxis);
+
+  //       svg
+  //         .append("g")
+  //         .attr("transform", "translate(0,60)")
+  //           .call(xAxis.ticks(d3.timeYear));
+
+  //       svg
+  //         .append("text")
+  //         .attr("transform", "translate(300,95)")
+  //         .style("text-anchor", "middle")
+  //         .attr("fill", "black")
+  //           .text("Dates");
+
+  //       svg
+  //         .selectAll("circle")
+  //         .data(Object.values(merged))
+  //         .enter()
+  //         .append("circle")
+  //         .attr("r", 5)
+  //           .attr("fill", "black")
+  //           .attr("cx", function (d) {
+  //               return xScale(d.points[0]);
+  //           })
+  //           .attr("cy", function (d) {
+  //               return yScale(d.points[1]);
+  //             })
+
+  //   }, [merged, count, justiceData]);
 
   return (
     <div id="svg-data">
